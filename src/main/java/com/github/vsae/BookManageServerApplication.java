9fe8df0acaefc19732c3726c4c9b2ce3b964804b
vsae8538 <vsae8538@gmail.com>
2021-02-26 19:29:15 +0800

    20210226 fix

--- src/main/java/com/github/vsae/BookManageServerApplication.java
+++ src/main/java/com/github/vsae/BookManageServerApplication.java
@@ -1,8 +1,13 @@
 package com.github.vsae;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@MapperScan(basePackages = "com.github.vsae" , annotationClass = Mapper.class)
+@ComponentScan({"com.github.vsae"})
 @SpringBootApplication
 public class BookManageServerApplication {
 
--- src/main/java/com/github/vsae/mapper/UserMapper.java
+++ src/main/java/com/github/vsae/mapper/UserMapper.java
@@ -3,15 +3,15 @@ package com.github.vsae.mapper;
 
 import com.github.vsae.model.User;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
-import org.mapstruct.Mapper;
 
 @Mapper
 public interface UserMapper {
 
     @Select("SELECT * FROM user WHERE username = #{userName} ")
-    public User getUserByUsername(String userName);
+    User getUserByUsername(String userName);
 
     @Insert("INSERT INTO user( " +
             "id, username, password, email) " +
@@ -20,9 +20,9 @@ public interface UserMapper {
     int insert(User user);
 
     @Select("SELECT * FROM user WHERE id = #{userId} ")
-    public User getUserById(Integer userId);
+    User getUserById(Integer userId);
 
     @Update("UPDATE user SET username = #{userName}, password = #{password}, email = #{email} " +
             "WHERE id = #{id} ")
-    public int update(User user);
+    int update(User user);
 }
